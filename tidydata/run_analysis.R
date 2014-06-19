bc55d553a9f3e2e813c38f51613266f2d263ceba
maganti <maganti0525@gmail.com>
2014-06-19 18:43:52 +0530

    Update run_analysis.R

--- tidydata/run_analysis.R
+++ tidydata/run_analysis.R
@@ -23,7 +23,7 @@
     testData_act$V1 <- factor(testData_act$V1,levels=activities$V1,labels=activities$V2)
     trainData_act$V1 <- factor(trainData_act$V1,levels=activities$V1,labels=activities$V2)
     
-#sets the names of the variable columns in the test and train data sets to the #ones read from the "features.text" file. 
+#sets the names of the variable columns in the test and train data sets to the ones read from the "features.text" file. 
 #Once again, reading in by setting colClasses to character, to properly extract the column names
 
     features <- read.table("features.txt",header=FALSE,colClasses="character")
@@ -49,7 +49,7 @@
     
 #Appropriately label the dataset with descriptive variable names. Following the
 #principles of tidy data, by changing all the column names to lowercase, replacing all 
-#alphanumeric character, replacing occurrence of "body" twice and #then setting the names 
+#alphanumeric character, replacing occurrence of "body" twice and then setting the names 
 #of the mergedData with the cleaned column names
 
     nams<-tolower(colnames(subdata))
@@ -58,12 +58,19 @@
     colnames(subdata)<-nams2
     
 #creating a second tidy data set with the average of each variable for each 
-# activity and each subject
+# activity and each subject. Many different options available to reshape the data, but preferred to use
+# melt and dcast. The same can be accomplished equally elegantly by using the "ddply" from the plyr package. 
+# Chose the subject, followed by activity ordering, where the rows are listed as subject 1 for all 
+# 6 activities, followed by subject 2 for all 6 activties and so on. For activity based ordering, simply swap the 
+#subject and activity in the dcast formula below
 
     mt<-melt(subdata, id.vars=c("subject", "activity"))
     cmt<-dcast(mt, subject+activity ~ variable, mean)
     
-#writes the resultant tidy data set to a file using write.csv
+#writes the resultant tidy data set to a file using write.csv. Setting row.names to false for cleaner reading 
+#this data set can be read back in using the read.csv, with stringsAsFactors set to false to preserve the order
+#in the factor variable. the same can be accomplished using the dput and dget function calls, without having to worry 
+#about factor level ordering
 
     write.csv(cmt, file="./tidydata.txt", row.names=F)
     tidydata<-read.csv("./tidydata.txt", stringsAsFactors=F)
